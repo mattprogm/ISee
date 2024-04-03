5974626ee44e0155109f12ecc4c03bff5c1ad7fa
Matheus Henrique <mattprogm@gmail.com>
2024-04-02 23:32:19 -0300

    feat: Added dependencies in Project
    Setup depencies and tweak de code settings for get then working.

---
+++ Pods/Target Support Files/Pods-ISee/Pods-ISee-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ISee : NSObject
+@end
+@implementation PodsDummy_Pods_ISee
+@end
---
+++ Pods/Target Support Files/Pods-ISeeTests/Pods-ISeeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ISeeTests : NSObject
+@end
+@implementation PodsDummy_Pods_ISeeTests
+@end
